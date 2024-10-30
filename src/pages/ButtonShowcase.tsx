f4f42bcd05f87453d36f4809719fce73787a9c37
N-i-ke <82163707+N-i-ke@users.noreply.github.com>
2024-10-31 00:13:54 +0900

    feat: Titleの変更とテキストの追加/背景色の変更
    サイトタイトルの変更とボタンの説明テキストの追加
    全体の背景色も付与

--- src/pages/ButtonShowcase.tsx
+++ src/pages/ButtonShowcase.tsx
@@ -5,10 +5,19 @@ import { Button } from "../components/Button"; // 正しい相対パスを指定
 const ButtonShowcase = () => {
   return (
     <div className="button-container">
-      <h1>Button Component</h1>
+      <h1>フロントエンド・ボタンコンポーネント（React・TypeScript）</h1>
+      <p>
+        ホバー時とフォーカス時のスタイルは全てのボタンに適用しています
+        <br />
+        ＊非活性時・ローディング以外
+      </p>
+      <p>テキストのみ</p>
       <Button label="Primary Button" />
+      <p>非活性時</p>
       <Button label="Secondary Button" disabled />
+      <p>ローディング時</p>
       <Button label="Loading Button" loading />
+      <p>リンク付き</p>
       <Button label="Link Button" href="https://example.com" />
     </div>
   );
