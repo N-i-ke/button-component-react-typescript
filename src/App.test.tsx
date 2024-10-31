95d949b23715bcbed53991fa749f39edb7b168e0
N-i-ke <82163707+N-i-ke@users.noreply.github.com>
2024-10-31 11:06:47 +0900

    fix: App.test.tsxのテストコードの修正
    テストコードの修正

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders ButtonShowcase component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(
+    /フロントエンド・ボタンコンポーネント（React・TypeScript）/i
+  );
+  expect(titleElement).toBeInTheDocument();
 });
